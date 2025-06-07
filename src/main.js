0378c6f7ad8fbcde8064d973f0b5a4eabef1c541
TwilightEch03s <lorenzokiri.uk@gmail.com>
2025-06-07 15:49:38 -0700

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ let config = {
     fps: {forceSetTimeOut: true, target: 60},
     width: 800,
     height: 600,
-    scene: [Load, RoomOne]
+    scene: [Load, RoomOne, RoomThree, RoomFour, RoomFive, RoomSix]
 
 }
 
