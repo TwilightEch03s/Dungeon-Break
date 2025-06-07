f6119d89b58d0d0741954496bd8c378a837ca2f9
TwilightEch03s <lorenzokiri.uk@gmail.com>
2025-06-07 15:52:49 -0700

    Player Movement Implementation

--- src/Scenes/RoomOne.js
+++ src/Scenes/RoomOne.js
@@ -1,6 +1,10 @@
 class RoomOne extends Phaser.Scene {
     constructor() {
         super("roomOneScene");
+        this.wKey = null;
+        this.aKey = null;
+        this.sKey = null;
+        this.dKey = null;
     }
 
     init() {
@@ -12,11 +16,24 @@ class RoomOne extends Phaser.Scene {
     }
 
     create() {
-
+        this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        
+        my.sprite.player = this.add.sprite(400, 300, "platformer_characters", "tile_0020.png");
+        my.sprite.player.setScale(3);
     }
 
     update() {
-
+        if (this.wKey.isDown) {
+            my.sprite.player.y -= 5;
+        } else if (this.sKey.isDown) {
+            my.sprite.player.y += 5;
+        } else if (this.aKey.isDown) {
+            my.sprite.player.x -= 5;
+        } else if (this.dKey.isDown) {
+            my.sprite.player.x += 5;
+        }
     }
-
 }
