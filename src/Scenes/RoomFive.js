f047276fc16fb6efd4d462d60597e53adeb0a56e
TwilightEch03s <lorenzokiri.uk@gmail.com>
2025-06-11 18:16:19 -0700

    Updated RoomFive

--- src/Scenes/RoomFive.js
+++ src/Scenes/RoomFive.js
@@ -29,22 +29,24 @@ class RoomFive extends Phaser.Scene {
         this.cracked = this.map.createLayer("Cracked", tilesets, 0, 0);
         this.decorations = this.map.createLayer("Decorations", tilesets, 0, 0);
 
+        // Player Collision
         this.wall.setCollisionByProperty({ collides: true });
-        this.decorations.setCollisionByProperty({ cracked: true });
 
+        // Inputs
         this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
+        // Player
         my.sprite.player = this.physics.add.sprite(712, 80, "platformer_characters", "tile_0006.png");
         my.sprite.player.setScale(0.8);
         my.sprite.player.body.setCollideWorldBounds(true);
 
+        // Player colliding
         this.physics.add.collider(my.sprite.player, this.wall);
-        this.physics.add.overlap(my.sprite.player, this.cracked, this.handleCrackeWalls, null, this);
-        this.physics.add.collider(my.sprite.player, this.decorations);
 
+        // Camera
         this.cameras.main.setZoom(this.SCALE);
         this.cameras.main.startFollow(my.sprite.player);
 
@@ -63,23 +65,37 @@ class RoomFive extends Phaser.Scene {
             velocityY = -1;
             player.anims.play('walk', true);
             this.isMoving = true;
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            my.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            my.vfx.walking.start();
         } else if (this.sKey.isDown) {
             velocityY = 1;
             player.anims.play('walk', true);
             this.isMoving = true;
-        } else if (this.aKey.isDown) {
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            my.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            my.vfx.walking.start();
+        } else if(this.aKey.isDown) {
             velocityX = -1;
             player.resetFlip();
             player.anims.play('walk', true);
             this.isMoving = true;
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            my.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            my.vfx.walking.start();
         } else if (this.dKey.isDown) {
             velocityX = 1;
             player.setFlip(true, false);
             player.anims.play('walk', true);
             this.isMoving = true;
-        } else {
+            my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            my.vfx.walking.setParticleSpeed(-this.PARTICLE_VELOCITY, 0);
+            my.vfx.walking.start();
+        }
+        else {
             player.anims.play('idle');
             this.isMoving = false;
+            my.vfx.walking.stop();
         }
 
         // Movement sfx
@@ -95,19 +111,5 @@ class RoomFive extends Phaser.Scene {
 
         const vec = new Phaser.Math.Vector2(velocityX, velocityY).normalize();
         player.setVelocity(vec.x * this.ACCELERATION, vec.y * this.ACCELERATION);
-    }
-
-    
-    handleCrackeWalls(player, tile) {
-                const tileProps = tile.properties || {};
-            if (tileProps.cracked && !this.transitioning && !this.wallBroke) {
-                this.transitioning = true;
-                this.wallBroke = true;
-                
-                this.cracked.visible = false;
-                
-                this.sound.play("wallcracked");
-                
-            }
     }
 }
